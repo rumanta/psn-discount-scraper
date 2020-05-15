357b60991c1609d466125337ba1e58be04e03588
William Rumanta <rumantawilliam@yahoo.co.id>
2020-05-16 05:40:31 +0700

    Add sort discount games before written to text

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+import re
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
-import re
-import time
 
 PSN_BASE_URL = 'https://store.playstation.com/'
 PSN_STORE_GAMES_URL = 'https://store.playstation.com/en-id/grid/STORE-MSF86012-GAMESALL/'
 LAST_PAGE_NUM = 156
-MINIMUM_DISCOUNT_THRESHOLD = 80
+MINIMUM_DISCOUNT_THRESHOLD = 70
 REQUEST_TIMEOUT = 15
 
 
@@ -39,7 +40,7 @@ def get_game_price(section):
     return title_text, game_link, price_before_text, price_after_text
 
 
-def game_metadata_writer(title_text, game_link, discount, price_before_text, price_after_text):
+def game_metadata_printer(title_text, game_link, discount, price_before_text, price_after_text):
     print(title_text)
     print(game_link)
     print(f'discount: {discount}%')
@@ -48,9 +49,30 @@ def game_metadata_writer(title_text, game_link, discount, price_before_text, pri
     print('\n')
 
 
+def game_metadata_writer(f, game_data):
+    f.write(f"{game_data['title']}\n")
+    f.write(f"{game_data['link']}\n")
+    f.write(f"discount: {game_data['discount']}%\n")
+    f.write(f"price before: {game_data['price_before']}\n")
+    f.write(f"price after: {game_data['price_after']}\n")
+    f.write('\n\n')
+
+
+def write_sorted_discount_game(discount_games):
+    sorted_discount_games = sorted(discount_games, key=lambda game: game['discount'], reverse=True)
+    f = open('discount_game_list.txt', 'w')
+    f.write(
+        f"PSN Discount Game List\ndate and time: {datetime.now().strftime('%m/%d/%Y - %H:%M:%S')}\n\n"
+    )
+    [game_metadata_writer(f, game_data) for game_data in sorted_discount_games]
+    f.close()
+
+
 def main():
+    discount_games = []
+
     for index_page in range(1, LAST_PAGE_NUM + 1):
-        print('Going in page', index_page)
+        print('Accessing page', index_page)
         try:
             page = requests.get(f'{PSN_STORE_GAMES_URL}{index_page}', timeout=REQUEST_TIMEOUT)
         except:
@@ -71,9 +93,21 @@ def main():
             discount = int(discount_numbers[0]) if len(discount_numbers) > 0 else -1
             if discount >= MINIMUM_DISCOUNT_THRESHOLD:
                 title_text, game_link, price_before_text, price_after_text = get_game_price(section)
-                game_metadata_writer(
-                    title_text, game_link, discount, price_before_text, price_after_text
-                )
+
+                # Uncomment this if you want to print the game info
+                # game_metadata_printer(
+                #     title_text, game_link, discount, price_before_text, price_after_text
+                # )
+                game_data = {
+                    'title': title_text,
+                    'link': game_link,
+                    'discount': discount,
+                    'price_before': price_before_text,
+                    'price_after': price_after_text,
+                }
+                discount_games.append(game_data)
+
+    write_sorted_discount_game(discount_games)
 
 
 if __name__ == '__main__':
